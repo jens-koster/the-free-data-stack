5ba31e3a83f2c443d0927e5414fa2b85d27cab41
Jens Köster <jens.koster@issuu.com>
2025-03-18 20:19:04 +0100

    cleaning up

--- setup.sh
+++ setup.sh
@@ -1,17 +1,20 @@
+# globals
+# a common folder for our lab data
 ln -s "$(pwd)/_data" /tmp/data
+# a folder where all logs are stores
+mkdir -p /tmp/logs
+# we need a common network so the containers can communicate
+# this is apprently persistent so no need torun this command but once.
+docker network create tfds-network
 
+# Airflow
 mkdir -p /tmp/logs/airflow
 mkdir -p /tmp/airflow
-mkdir -p /tmp/logs
-
-mkdir -p /tmp/logs/notebook-runner
-mkdir -p /tmp/notebooks
-mkdir -p /tmp/output_notebooks
-
-
-
 ln -s "$(pwd)/Airflow/config" /tmp/airflow/config
 ln -s "$(pwd)/Airflow/plugins" /tmp/airflow/plugins
 ln -s "$(pwd)/Airflow/dags" /tmp/airflow/dags
 
-docker network create tfds-network
+# Papermill
+mkdir -p /tmp/logs/book-runner
+mkdir -p /tmp/notebooks
+mkdir -p /tmp/output_notebooks
