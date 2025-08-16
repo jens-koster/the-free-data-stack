3b2938c93c0867f2e7c26dbe248d12f393ff2bfc
Jens Köster <jens.koster@issuu.com>
2025-08-16 14:40:12 +0200

    compose-<env var

--- spark/build.sh
+++ spark/build.sh
@@ -15,26 +15,6 @@ echo "re-installing spark requirements for this venv"
 pip uninstall -r requirements.txt -y
 pip install -r requirements.txt
 
-echo "🔄 Auto incrementing version number..."
-
-# IMAGE_PREFIX="freeds/spark-base:1.0."
-# FILE="docker-compose.yaml"
-
-# # Extract the current version number
-# current_version=$(grep -oE "${IMAGE_PREFIX}[0-9]+" "$FILE" | sed "s|${IMAGE_PREFIX}||" | head -n 1)
-
-# if [[ -z "$current_version" ]]; then
-#   echo "❌ Could not find a version like ${IMAGE_PREFIX}x in $FILE"
-#   exit 1
-# fi
-
-# # Increment version
-# new_version=$((current_version + 1))
-
-# # Replace in the file using '|' as delimiter
-# sed -i.bak "s|${IMAGE_PREFIX}${current_version}|${IMAGE_PREFIX}${new_version}|g" "$FILE"
-# echo "✅ Updated docker image in $FILE: ${IMAGE_PREFIX}${current_version} → ${IMAGE_PREFIX}${new_version}"
-
 source jar.sh
 
 echo building docker image
@@ -54,6 +34,6 @@ freeds docker compose up -d --remove-orphans
 
 echo "building papermill"
 
-cd ../papermill
+cd ../jupyter
 source build.sh
 cd ../spark
