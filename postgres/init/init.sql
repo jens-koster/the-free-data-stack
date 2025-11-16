33c3113132a1de41c226415d1686b8a39870dba0
Jens Köster <jens.koster@issuu.com>
2025-11-16 21:11:23 +0100

    kafka connect added

--- postgres/init/init.sql
+++ postgres/init/init.sql
@@ -1,4 +1,18 @@
 --  docker exec -it postgres psql -U airflow -d postgres -f /docker-entrypoint-initdb.d/init.sql
 create database airflow;
 create database iceberg;
+create database jaffle_shop;
 SELECT datname as database_name FROM pg_database WHERE datistemplate = false;
+
+
+
+
+/*
+CREATE USER kafka WITH PASSWORD 'jazzword';
+
+CREATE DATABASE kafka_db OWNER kafka;
+
+GRANT ALL PRIVILEGES ON DATABASE kafka_db TO kafka;
+GRANT USAGE ON SCHEMA public TO kafka;
+GRANT CREATE ON SCHEMA public TO kafka;
+*/
